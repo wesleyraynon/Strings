e96b8bf22dfe988e12016182db8162d4e034cd55
wesleyHW <freire.wesley@escola.pr.gov.br>
2025-07-08 14:51:22 +0000

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+ botaoMostraPalavras.addEventListener('click',mostraPalavrasChave);
+function mostraPalavrasChave (){
+    const texto = document.querySelector('#entrada-de-texto') .value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
+    campoResultado.textContent = palavras.join(",");
+}
